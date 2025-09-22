ddfd8a0a97efc810db80d858d9a89274b531468b
Qiandai Huang <qiandaihuang@gmail.com>
2025-09-23 00:10:21 +1000

    fixed to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <App />
+    <Router basename="/csesoc-exec">
+    <App />
     </Router>
   </StrictMode>,
 )
